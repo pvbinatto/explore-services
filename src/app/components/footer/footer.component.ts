b1d8f4d0782490126a76a048d7c0bf1b493bb035
pvbinatto <paulobinatto@gmail.com>
2022-06-27 20:08:31 -0300

    Feat: Footer finalizado

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   constructor() { }
-
+  fullYear = new Date().getFullYear();
   ngOnInit(): void {
+    
   }
 
 }
